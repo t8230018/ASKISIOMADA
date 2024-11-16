043893d3f1e3f40cf64e0c529d547e8757d41394
t8230018 <t8230018@aueb.gr>
2024-11-16 13:44:58 +0200

    Update EmfanisiAm.java

--- EmfanisiAm.java
+++ EmfanisiAm.java
@@ -2,5 +2,6 @@ public class EmfanisiAm {
     public static void main(String args[]) {
         System.out.println(8220039);
         System.out.println(8230151);
+        System.out.println(8230018);
     }
 }
